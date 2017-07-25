0baca5ed68dfbd69543070d941c3a3f07b1e79f6
Sean Dunn <seandunn@Seans-MBP.fios-router.home>
2017-07-24 21:01:27 -0400

    add video list item component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD5TsqwTCCaRUXQDRQMKkAU5C-M_5zcWa4';
+
 // Create a new component. This compenent should produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // this is the same thing as {videos: videos}
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 };
 
 // Take this component's generated html and put it on the page
